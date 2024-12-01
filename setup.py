ca12558bf127097819a27a9bb4e8bb6f3da1925c
amberhu8 <amberhu63@gmail.com>
2024-11-30 20:45:15 -0800

    add packaging support

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gpslds',
+    version='0.1',
+    description='Gaussian process switching linear dynamical systems',
+    packages=find_packages(),
+    python_requires='>=3.9',
+    )
